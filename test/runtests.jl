6db17291778f1f45fc9c45d49beb35f3fa2c66ae
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-01-28 14:27:31 -0500

    adding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,30 +2,46 @@ using DyadicKDE
 using Test
 using Random
 
-@testset "small_example" begin
+@testset "Confidence bands" begin
+
+    Random.seed!(314159)
 
     # specify parameters
-    n_data = 50
+    n_data = 80
     kernel_names = ["epanechnikov_order_2", "epanechnikov_order_4"]
     evals = collect(range(-2.0, stop=2.0, length=10))
     sdp_solver = "cosmo"
     n_resample = 1000
-    significance_level = 0.05
+    significance_level = 0.5
     p = [0.25, 0.0, 0.75]
 
-    # make data and get bandwidth
-    data = make_data(n_data, p)
-    h_ROT = estimate_ROT_bandwidth(data, "epanechnikov_order_2")
+    for rep in 1:10
 
-    # run experiments
-    Random.seed!(314159)
-    for kernel_name in kernel_names
+        # make data and get bandwidth
+        data = make_data(n_data, p)
+        h_ROT = estimate_ROT_bandwidth(data, "epanechnikov_order_2")
 
-        est = DyadicKernelDensityEstimator(
-            kernel_name, h_ROT, significance_level,
-            n_resample, sdp_solver, evals, data, Dict())
+        # run experiments
+        for kernel_name in kernel_names
 
-        fit(est)
+            est = DyadicKernelDensityEstimator(
+                kernel_name, h_ROT, significance_level,
+                n_resample, sdp_solver, evals, data, Dict())
+
+            fit(est)
+
+            @test all(
+                0
+                .<= est.bci[1,:]
+                .<= est.ucb[1,:]
+                .<= est.pci[1,:]
+                .<= est.fhat
+                .<= est.pci[2,:]
+                .<= est.ucb[2,:]
+                .<= est.bci[2,:]
+            )
+
+        end
 
     end
 
