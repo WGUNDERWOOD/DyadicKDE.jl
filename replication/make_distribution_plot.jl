118f70a9850d4e6bf4c71722ed592e69200288e9
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-02-15 15:17:42 -0500

    adding new plot scripts

---
+++ replication/make_distribution_plot.jl
@@ -0,0 +1,54 @@
+# generate the distribution plots found in
+# https://arxiv.org/abs/2201.05967
+
+using DyadicKDE
+include("./plot_helpers.jl")
+
+# specify parameters
+n_evals = 100
+degeneracies = ["total", "partial", "none"]
+evals = collect(range(-2.0, stop=2.0, length=n_evals))
+ps = Dict(
+    "total" => [0.5, 0.0, 0.5],
+    "partial" => [0.25, 0.0, 0.75],
+    "none" => [0.2, 0.2, 0.6],
+)
+
+
+println("Making plots")
+linewidth = 1.0
+y_lim = [-0.01, 0.45]
+handle_f = PyPlot.matplotlib.lines.Line2D([0], [0], color="k", lw=linewidth, label="\$f_W(w)\$")
+handle_Var_f_given_A = PyPlot.matplotlib.lines.Line2D([0], [0], color="k", lw=linewidth,
+    label="\$\\mathrm{Var}[f_{W \\mid A}(w \\mid A_i)]^{1/2}\$")
+handles = [handle_f, handle_Var_f_given_A]
+
+
+for degen in degeneracies
+
+    p = ps[degen]
+    f = generate_f(evals, p)
+    Var_f_given_A = generate_Var_f_given_A(evals, p)
+
+    # plot f
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.plot(evals, f, color = "black", linewidth=linewidth)
+
+    # plot Var_f_given_A
+    ax.plot(evals, Var_f_given_A.^(0.5),
+        color = "black", linewidth=linewidth, linestyle=(0, (1,1)))
+
+    # save plot
+    PyPlot.xlabel("Evaluation point, \$w\$")
+    plt.ylim(y_lim)
+    x_min = minimum(evals)
+    x_max = maximum(evals)
+    plt.xlim((x_min, x_max))
+    plt.yticks(range(0.0, stop=0.4, step=0.1))
+    legend(handles=handles, loc="upper left")
+    plt.ylabel("Density", labelpad=4.0)
+    plt.tight_layout()
+    PyPlot.savefig("distribution_plot_$degen.pdf")
+    close("all")
+
+end
---
+++ replication/make_trade_plot.jl
@@ -0,0 +1,92 @@
+using CSV
+using DataFrames
+using LinearAlgebra
+using PyPlot
+using DyadicKDE
+
+
+
+
+
+# set estimation parameters
+n_evals = 100 # 200 is CKS
+kernel_name = "epanechnikov_order_4"
+evals = collect(range(-10.0, stop=10.0, length=n_evals))
+sdp_solver = "mosek"
+n_resample = 10000
+significance_level = 0.05
+
+
+
+# set plot parameters
+linewidth = 1.0
+y_lim = [-0.003, 0.08]
+
+
+e = 0.22
+pci_marker = PyPlot.matplotlib.path.Path([[-1,0], [-1,e], [-1,-e], [-1,0], [1,0], [1,e], [1,-e], [1,0]])
+handle_fhat = PyPlot.matplotlib.lines.Line2D([0], [0], color="k", lw=linewidth, linestyle=(0, (1,1)), label="\$\\widehat f_W(w)\$")
+handle_ucb = PyPlot.matplotlib.patches.Patch(facecolor="lightgray", edgecolor="lightgray", label="UCB")
+handle_pci = PyPlot.matplotlib.lines.Line2D([0], [0], color="black", lw=0, markersize=17, marker=pci_marker, label="PCI")
+handles = [handle_fhat, handle_ucb, handle_pci]
+
+
+
+# run experiments
+
+for year in ["1995", "2000", "2005"]
+
+    # load data
+    println(year)
+    data_frame = CSV.read("../../data/data$year.csv", DataFrame)
+    data_array = Array(data_frame[:,2:end])
+    data = UpperTriangular(data_array + data_array') ./ 2
+    n = size(data, 1)
+    N = 0.5 * n * (n-1)
+    nz = sum(data .== -Inf)
+    nnz = N - nz
+    println("Number of nodes n: $n")
+    println("Number of total edges N: $N")
+    println("Number of non-zero samples: $nnz")
+    println("Proportion of non-zero samples: $(nnz/N)")
+
+    # get bandwidth
+    h_ROT = estimate_ROT_bandwidth(data, "epanechnikov_order_2")
+    println("ROT bandwidth: $h_ROT")
+    data[data .== -Inf] .= -1e10
+
+    # fit dyadic kernel density estimator
+    est = DyadicKernelDensityEstimator(
+        kernel_name, h_ROT, significance_level,
+        n_resample, sdp_solver, evals, data,
+        Dict("year" => year))
+
+    fit(est)
+
+    # plot fhat
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.plot(est.evals, est.fhat,
+        color = "black", linewidth=linewidth, linestyle=(0, (1,1)))
+
+    # plot ucb
+    ax.fill_between(est.evals, est.ucb[1,:], est.ucb[2,:],
+        color="lightgray", linewidth=0.0)
+
+    # plot pci
+    plot_confidence_intervals(ax, est.evals, est.pci[1,:],
+        est.pci[2,:], 10, "black", linewidth, "-")
+
+    # save plot
+    PyPlot.xlabel("Bilateral trade volume")
+    plt.ylim(y_lim)
+    x_min = minimum(est.evals)
+    x_max = maximum(est.evals)
+    plt.xlim((x_min, x_max))
+    plt.yticks(range(0.0, stop=0.08, step=0.01))
+    legend(handles=handles, loc="upper left")
+    plt.ylabel("Density", labelpad=4.0)
+    plt.tight_layout()
+    PyPlot.savefig("../../plots/trade_$year.pdf")
+    close("all")
+
+end
